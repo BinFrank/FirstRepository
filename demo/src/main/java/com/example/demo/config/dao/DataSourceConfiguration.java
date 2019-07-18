61214e54f32a6154fe96bc528fd63c007a31793f
BinFrank <gxb55212@163.com>
2019-07-18 10:29:06 +0800

    update datasource_pool

--- demo/src/main/java/com/example/demo/config/dao/DataSourceConfiguration.java
+++ demo/src/main/java/com/example/demo/config/dao/DataSourceConfiguration.java
@@ -1,35 +1,103 @@
 package com.example.demo.config.dao;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.alibaba.druid.pool.DruidDataSource;
 
-import java.beans.PropertyVetoException;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import com.example.demo.handler.GlobalExceptionHandler;
 
 @Configuration
 //配置mybatis mapper的扫描路径
 @MapperScan("com.example.demo.dao")
 public class DataSourceConfiguration {
-    @Value("${jdbc.driver}")
+    @Value("${spring.datasource.driver-class-name}")
     private String jdbcDriver;
-    @Value("${jdbc.url}")
+
+    @Value("${spring.datasource.url}")
     private String jdbcUrl;
-    @Value("${jdbc.username}")
+
+    @Value("${spring.datasource.username}")
     private String jdbcUser;
-    @Value("${jdbc.password}")
+
+    @Value("${spring.datasource.password}")
     private String jdbcPassword;
 
-    @Bean(name = "dataSource")
-    public ComboPooledDataSource createDateSource() throws PropertyVetoException {
-        ComboPooledDataSource dataSource = new ComboPooledDataSource();
-        dataSource.setDriverClass(jdbcDriver);
-        dataSource.setJdbcUrl(jdbcUrl);
-        dataSource.setUser(jdbcUser);
-        dataSource.setPassword(jdbcPassword);
-        // 关闭连接后不自动commit
-        dataSource.setAutoCommitOnClose(false);
-        return dataSource;
+    @Value("${spring.datasource.initialSize}")
+    private int initialSize;
+
+    @Value("${spring.datasource.minIdle}")
+    private int minIdle;
+
+    @Value("${spring.datasource.maxActive}")
+    private int maxActive;
+
+    @Value("${spring.datasource.maxWait}")
+    private int maxWait;
+
+    @Value("${spring.datasource.timeBetweenEvictionRunsMillis}")
+    private int timeBetweenEvictionRunsMillis;
+
+    @Value("${spring.datasource.minEvictableIdleTimeMillis}")
+    private int minEvictableIdleTimeMillis;
+
+    @Value("${spring.datasource.validationQuery}")
+    private String validationQuery;
+
+    @Value("${spring.datasource.testWhileIdle}")
+    private boolean testWhileIdle;
+
+    @Value("${spring.datasource.testOnBorrow}")
+    private boolean testOnBorrow;
+
+    @Value("${spring.datasource.testOnReturn}")
+    private boolean testOnReturn;
+
+    @Value("${spring.datasource.poolPreparedStatements}")
+    private boolean poolPreparedStatements;
+
+    @Value("${spring.datasource.maxPoolPreparedStatementPerConnectionSize}")
+    private int maxPoolPreparedStatementPerConnectionSize;
+
+    @Value("${spring.datasource.filters}")
+    private String filters;
+
+    @Value("{spring.datasource.connectionProperties}")
+    private String connectionProperties;
+
+    @Bean("dataSource")     //声明其为Bean实例
+    public DataSource dataSource(){
+        DruidDataSource datasource = new DruidDataSource();
+
+        datasource.setUrl(this.jdbcUrl);
+        datasource.setUsername(jdbcUser);
+        datasource.setPassword(jdbcPassword);
+        datasource.setDriverClassName(jdbcDriver);
+
+        //configuration
+        datasource.setInitialSize(initialSize);
+        datasource.setMinIdle(minIdle);
+        datasource.setMaxActive(maxActive);
+        datasource.setMaxWait(maxWait);
+        datasource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        datasource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        datasource.setValidationQuery(validationQuery);
+        datasource.setTestWhileIdle(testWhileIdle);
+        datasource.setTestOnBorrow(testOnBorrow);
+        datasource.setTestOnReturn(testOnReturn);
+        datasource.setPoolPreparedStatements(poolPreparedStatements);
+        datasource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
+        try {
+            datasource.setFilters(filters);
+        } catch (SQLException e) {
+            new GlobalExceptionHandler();
+        }
+        datasource.setConnectionProperties(connectionProperties);
+
+        return datasource;
     }
 }
